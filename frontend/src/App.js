f94e694d876a1f7f8aef0c8843032e792739c59e
Karim Arkia <karimarkia@gmail.com>
2021-03-29 16:02:51 +0300

    working on header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
-    <div>
-      Welcome to proShop App 
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to proShop App</h1>
+      </main>
+      <Footer />
+    </>
   )
 }
 

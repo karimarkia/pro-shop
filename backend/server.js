4437e92752680c5134e974df8b6a1665e278c3bc
Karim Arkia <karimarkia@gmail.com>
2021-04-01 11:01:29 +0300

    start working on mongoose schema

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const products = require('./data/products')
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
-app.get('/',(req,res)=>{
-    res.send('Api is running...')
+dotenv.config()
+// connect to the DB
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('Api is running...')
 })
 
-app.get('/api/product',(req,res)=>{
-    res.json(products)
+app.get('/api/product', (req, res) => {
+  res.json(products)
 })
 
-
-app.get('/api/product/:id',(req,res)=> {
-    const product = products.find(p=> p._id === req.params.id)
-    res.json(product)
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
 })
 
-
-
-app.listen(5000,console.log('Server running at 5000'))
+const PORT = process.env.PORT || 5000
+ 
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
